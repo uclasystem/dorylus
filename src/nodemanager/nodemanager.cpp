0c5fb31fb6d69f5d160f56fe499397f906e6e56a
thorpedoes <thorpedoes@g.ucla.edu>
2019-06-28 23:16:12 +0000

    BAR log statements added to nodemanager to find segfault

--- src/nodemanager/nodemanager.cpp
+++ src/nodemanager/nodemanager.cpp
@@ -7,7 +7,6 @@
 
 Node NodeManager::me;
 unsigned NodeManager::masterIdx = 0;
-//bool NodeManager::master;
 std::vector<Node> NodeManager::allNodes;
 unsigned NodeManager::numLiveNodes;
 pthread_mutex_t NodeManager::mtx_waiter;
@@ -287,17 +286,23 @@ void NodeManager::barrierCB(const char* path) {
 void NodeManager::barrier(const char* bar) {
     fprintf(stderr, "BAR Start\n");
     inBarrier = true;
+
+    fprintf(stderr, "BAR context: %s\n", bar);
+    
+    fprintf(stderr, "BAR bContext\n");
     BarrierContext bContext(bar);
     std::string barName = bContext.path;
     pthread_mutex_lock(&mtx_appBarriers);
-    //assert(appBarriers.find(barName) == appBarriers.end());
+    fprintf(stderr, "BAR barname: %s\n", barName.c_str());
+    fprintf(stderr, "BAR Overwriting barrier\n");
     appBarriers[barName] = bContext;    // Overwrite barrier
+    fprintf(stderr, "BAR unlock appBarriers\n");
     pthread_mutex_unlock(&mtx_appBarriers);
 
-    fprintf(stderr, "B1\n");
+    fprintf(stderr, "BAR Master check\n");
 
     if(me.master) {
-	fprintf(stderr, "BarJon\n");
+	fprintf(stderr, "BAR Jon\n");
         createNode(barName.c_str(), false, true, &createCB);
         //fprintf(stderr, "Node %s: Created barrier %s\n", me.name.c_str(), barName.c_str());
 
@@ -324,7 +329,7 @@ void NodeManager::barrier(const char* bar) {
          */
         ZKInterface::recursiveDeleteZKNode(barName.c_str());
     } else {
-	fprintf(stderr, "BarChristian\n");
+	fprintf(stderr, "BAR Christian\n");
         fprintf(stderr, "Node %s: Waiting to enter barrier %s\n", me.name.c_str(), barName.c_str());
         pthread_mutex_lock(&mtx_waiter);
         if(ZKInterface::checkZKExists(barName.c_str(), checkBarrierExists) == false)
