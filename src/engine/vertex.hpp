06199770ca2ef1085e13301f74037cae1376e7ef
Jose <huguanzhou123@sina.com>
2019-07-21 14:52:49 -0700

    Reorganize include dependencies and file names

--- src/engine/vertex.hpp
+++ src/engine/vertex.hpp
@@ -70,4 +70,57 @@ class Vertex {
         void setParent(IdType p); 
 };
 
+
+template <typename VertexType>
+class GhostVertex {
+    public:
+        
+        // Use a vector to make data in old iterations persistent.
+        std::vector<VertexType> vertexData;
+
+        //char version;
+        RWLock lock;
+        int32_t degree;
+
+        std::vector<IdType> outEdges;
+
+        GhostVertex() {
+            lock.init();
+            degree = 0;
+        }
+
+        GhostVertex(const VertexType vData) {
+            lock.init();
+            degree = 0;
+            vertexData.clear();
+            vertexData.push_back(vData);
+        }
+
+        ~GhostVertex() {
+            lock.destroy();
+        }
+
+        VertexType data() {     // Get the current value.
+            lock.readLock();
+            VertexType vData = vertexData.back();
+            lock.unlock();
+            return vData;
+        }
+
+        void setData(VertexType* value) {   // Modify the current value.
+            lock.writeLock();
+            vertexData.back() = *value;
+            lock.unlock();
+        }
+
+        void addData(VertexType* value) {   // Add a new value of the new iteration.
+            lock.writeLock();
+            vertexData.push_back(*value);
+            lock.unlock();
+        }
+
+        void incrementDegree() { ++degree; }
+};
+
+
 #endif /* __VERTEX_H__ */
