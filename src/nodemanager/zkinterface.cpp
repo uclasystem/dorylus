1638c1685a0515a49d983facad1a4e740a1643d9
Jose <huguanzhou123@sina.com>
2019-07-22 16:57:39 -0700

    Cleaned up node manager

--- src/nodemanager/zkinterface.cpp
+++ src/nodemanager/zkinterface.cpp
@@ -6,6 +6,46 @@
 #include "../utils/utils.hpp"
 
 
+/** Helper function: convert from type enum to string. */
+const char *
+type2String(int state) {
+    if (state == ZOO_CREATED_EVENT)
+        return "CREATED_EVENT";
+    if (state == ZOO_DELETED_EVENT)
+        return "DELETED_EVENT";
+    if (state == ZOO_CHANGED_EVENT)
+        return "CHANGED_EVENT";
+    if (state == ZOO_CHILD_EVENT)
+        return "CHILD_EVENT";
+    if (state == ZOO_SESSION_EVENT)
+        return "SESSION_EVENT";
+    if (state == ZOO_NOTWATCHING_EVENT)
+        return "NOTWATCHING_EVENT";
+
+    return "UNKNOWN_EVENT_TYPE";
+}
+
+
+/** Helper function: convert from state enum to string. */
+const char *
+state2String(int state) {
+    if (state == 0)
+        return "CLOSED_STATE";
+    if (state == ZOO_CONNECTING_STATE)
+        return "CONNECTING_STATE";
+    if (state == ZOO_ASSOCIATING_STATE)
+        return "ASSOCIATING_STATE";
+    if (state == ZOO_CONNECTED_STATE)
+        return "CONNECTED_STATE";
+    if (state == ZOO_EXPIRED_SESSION_STATE)
+        return "EXPIRED_SESSION_STATE";
+    if (state == ZOO_AUTH_FAILED_STATE)
+        return "AUTH_FAILED_STATE";
+
+    return "INVALID_STATE";
+}
+
+
 /** Extern class-wide fields. */
 zhandle_t *ZKInterface::zh = NULL;
 clientid_t ZKInterface::myId;
