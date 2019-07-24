bff05d76161badcc7854ce9bb93a9c2c96feef4a
Jose <huguanzhou123@sina.com>
2019-07-23 19:57:10 -0700

    A lot of changes for the data storage logic. Debugging...

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cassert>
+#include "engine/engine.hpp"
+
+
+using namespace std;
+
+
+/**
+ *
+ * Main entrance of the aggregate benchmark.
+ * 
+ */
+int
+main(int argc, char *argv[]) {
+    initArgs();     // Currently useless.
+    parseArgs(&argc, argv);
+
+    // Initialize the engine.
+    std::vector<unsigned> layerConfig(5, 3);
+    Engine::init(argc, argv, layerConfig);
+
+    // Start one run of the engine.
+    Engine::run();
+
+    // Procude the output files.
+    Engine::output();
+
+    // Destroy the engine.
+    Engine::destroy();
+
+    return 0;
+}
