a725594d7316fe66dcca1957aeef49a9822d8cb8
Jose <huguanzhou123@sina.com>
2019-07-25 16:50:35 -0700

    Trying to run with lambdas

--- src/commmanager/lambda_comm.hpp
+++ src/commmanager/lambda_comm.hpp
@@ -42,7 +42,7 @@ parse(const char* buf, int32_t offset) {
 }
 
 // ID represents either layer or data partition, depending on server responding.
-void
+static void
 populateHeader(char* header, int32_t op, int32_t id, int32_t rows = 0, int32_t cols = 0) {
     serialize<int32_t>(header, 0, op);
     serialize<int32_t>(header, 1, id);
