0a6b8eb5e4bb2abac516d995f72b901b02c15301
Jose <huguanzhou123@sina.com>
2019-07-29 16:18:51 -0700

    Test graph too small

--- src/graph-server/engine/engine.cpp
+++ src/graph-server/engine/engine.cpp
@@ -296,7 +296,7 @@ Engine::worker(unsigned tid, void *args) {
                 //////////////////////////////////
                 Node nodeMe = NodeManager::getNode(nodeId);
                 LambdaComm lambdaComm(verticesDataBuf, nodeMe.pubip, dataserverPort, graph.getNumLocalVertices(),
-                                      getNumFeats(),getNumFeats(iteration + 1), graph.getNumLocalVertices() < 5 ? graph.getNumLocalVertices() 5, 1);
+                                      getNumFeats(),getNumFeats(iteration + 1), graph.getNumLocalVertices() < 5 ? graph.getNumLocalVertices() : 5, 1);
                 
                 // Create and launch the sender & receiver workers.
                 std::thread t([&] {
