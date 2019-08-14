44e4ae40ae375c13e77a3bc98a0840025bfee7b9
Shen Teng <shenteng@engineering.ucla.edu>
2019-08-14 16:43:42 -0700

    Modify it to OOP fashion

---
+++ funcs/forward-prop-GPU/CuMatrix.hpp
@@ -0,0 +1,98 @@
+#ifndef __CUMATIX_HPP__
+#define __CUMATIX_HPP__
+#include "cublas_v2.h"
+#include "../../src/utils/utils.hpp"
+#include <cstring>
+#include <memory>
+
+using std::endl;
+using std::cout;
+class CuMatrix : public Matrix
+{
+public:
+	CuMatrix(){};
+	CuMatrix(const CuMatrix& M);
+	CuMatrix(const Matrix& M, cublasHandle_t & handle_);	
+	~CuMatrix(); //will run for a long time. It's better to collect memory
+	void updateMatrixFromGPU();
+	CuMatrix dot(const CuMatrix& M);
+
+	// friend class ComputingUnit; 
+
+private:
+	void deviceMalloc();
+	void deviceSetMatrix();
+
+	cublasHandle_t handle;
+	cudaError_t cudaStat;
+	cublasStatus_t stat;
+	float * devPtr;
+};
+
+CuMatrix::CuMatrix(const Matrix& M, cublasHandle_t & handle_)
+	:Matrix(M.rows,M.cols,M.data){
+	handle=handle_;
+	deviceMalloc();
+	deviceSetMatrix();
+}
+
+void CuMatrix::deviceMalloc(){
+    cudaStat = cudaMalloc ((void**)&devPtr, rows*cols*sizeof(float));
+    if (cudaStat != cudaSuccess) {
+        printf ("device memory allocation failed\n");
+        exit (EXIT_FAILURE);
+    }
+}
+void CuMatrix::deviceSetMatrix(){
+	stat = cublasSetMatrix (rows,cols, sizeof(float), data, rows , devPtr, rows);
+    if (stat != CUBLAS_STATUS_SUCCESS) {
+        printf ("data download failed\n");
+        cudaFree (devPtr);
+        cublasDestroy(handle);
+        exit (EXIT_FAILURE);
+    }
+}
+
+void CuMatrix::updateMatrixFromGPU(){
+	stat = cublasGetMatrix (rows, cols, sizeof(float), devPtr, rows, data, rows );
+    if (stat != CUBLAS_STATUS_SUCCESS) {
+        printf ("data upload failed\n");
+        cudaFree (devPtr);
+        cublasDestroy(handle);
+        exit (EXIT_FAILURE);
+    }
+}
+
+CuMatrix::~CuMatrix(){
+	cudaFree (devPtr);
+	delete[] data; 
+}
+
+CuMatrix CuMatrix::dot(const CuMatrix& M){
+	if(handle!=M.handle){
+		std::cout<<"Handle don't match\n";
+		exit(EXIT_FAILURE);
+	}
+	Matrix mat_C=Matrix(rows,M.cols,new float[rows*M.cols*sizeof(float)]);
+	CuMatrix C(mat_C,handle);
+	float alpha=1.0f;
+    float beta=0.0f;
+    //1. cublas is using col-major
+    //2. when cpy into/out device memory, it will do Transpose 
+    //3. C=AB and C^T= (B^T*A^T)
+    //This means just swap the order of multiplicaiton
+    //Guide: https://peterwittek.com/cublas-matrix-c-style.html
+	cublasSgemm(handle,
+   		CUBLAS_OP_N,CUBLAS_OP_N,
+    	M.cols,rows,cols,
+    	&alpha,
+    	M.devPtr,M.cols,
+    	devPtr,cols,
+    	&beta,
+    	C.devPtr,M.cols);
+	return C;
+}
+
+
+
+#endif
--- funcs/forward-prop-GPU/comp_unit.hpp
+++ funcs/forward-prop-GPU/comp_unit.hpp
@@ -1,6 +1,7 @@
 #ifndef __COMP_UNIT_HPP__
 #define __COMP_UNIT_HPP__
 
+#include <cstring>
 #include <chrono>
 #include <iostream>
 #include <memory>
@@ -8,104 +9,43 @@
 #include <string>
 #include <thread>
 #include <cmath>
-#include <cstring>
-// #include <cstdlib>
-// #include <cmath>
+#include <cstdlib>
+#include <cmath>
 #include <cuda_runtime.h>
 #include "cublas_v2.h"
 #include "../../src/utils/utils.hpp"
-#include <memory>
+#include "CuMatrix.hpp"
+#include <thrust/reduce.h>
 
-
-
-float* deviceMalloc(const Matrix& M){
-	float* devPtr;
-	cudaError_t cudaStat;
-    cudaStat = cudaMalloc ((void**)&devPtr, M.rows*M.cols*sizeof(float));
-    if (cudaStat != cudaSuccess) {
-        printf ("device memory allocation failed");
-        exit (EXIT_FAILURE);
-    }
-    return devPtr;
-}
-void deviceSetMatrix(cublasHandle_t &handle,const Matrix& M, float* devPtr ){
-	cublasStatus_t stat;
-	stat = cublasSetMatrix (M.rows, M.cols, sizeof(float), M.data, M.rows , devPtr, M.rows);
-    if (stat != CUBLAS_STATUS_SUCCESS) {
-        printf ("data download failed");
-        cudaFree (devPtr);
-        cublasDestroy(handle);
-        exit (EXIT_FAILURE);
-    }
-}
-
-void deviceFetchMatrix(float* devPtr,Matrix M){
-	cublasStatus_t stat = cublasGetMatrix (M.rows, M.cols, sizeof(float), devPtr, M.rows, M.data, M.rows );
-    if (stat != CUBLAS_STATUS_SUCCESS) {
-        printf ("data upload failed");
-        cudaFree (devPtr);
-        // cublasDestroy(handle);
-        exit (EXIT_FAILURE);
-    }
-}
-
+//It maintains a GPU context
 class ComputingUnit
 {
 public:
-	ComputingUnit(){};
+	ComputingUnit();
 	Matrix dot(Matrix& A,Matrix& B); 	// use cublas for speed
-	Matrix activate(const Matrix& A); //probably use thrust. Since its not a Linear Algebra op
-	~ComputingUnit(){};
+	Matrix activate(const Matrix& A){}; //probably use thrust. Since its not a Linear Algebra op
+	~ComputingUnit(){cublasDestroy(handle);}
+
+private:
+	cublasHandle_t handle;
+	cublasStatus_t stat;
 };
 
-Matrix ComputingUnit::dot(Matrix& A, Matrix& B){
-    cublasHandle_t handle;
-    Matrix C(A.rows,B.cols,new float[A.rows*B.cols]);
-    float* devPtrA=deviceMalloc(A);
-    float* devPtrB=deviceMalloc(B);
-    float* devPtrC=deviceMalloc(C);
-   	
-   	cublasStatus_t stat;
+ComputingUnit::ComputingUnit(){
     stat = cublasCreate(&handle);
     if (stat != CUBLAS_STATUS_SUCCESS) {
         printf ("CUBLAS initialization failed\n");
         exit (EXIT_FAILURE);
     }
-    
-    deviceSetMatrix(handle,A,devPtrA);
-    deviceSetMatrix(handle,B,devPtrB);
-    deviceSetMatrix(handle,C,devPtrC);
-
-    float alpha=1.0f;
-    float beta=0.0f;
-    //1. cublas is using col-major
-    //2. when cpy into/out device memory, it will do Transpose 
-    //3. C=AB and C^T= (B^T*A^T)
-    //This means just swap the order of multiplicaiton
-    //Guide: https://peterwittek.com/cublas-matrix-c-style.html
-   	cublasSgemm(handle,
-   		CUBLAS_OP_N,CUBLAS_OP_N,
-    	B.cols,A.rows,A.cols,
-    	&alpha,
-    	devPtrB,B.cols,
-    	devPtrA,A.cols,
-    	&beta,
-    	devPtrC,B.cols);
-
-    stat = cublasGetMatrix (C.rows, C.cols, sizeof(float), devPtrC, C.rows, C.data, C.rows );
-    if (stat != CUBLAS_STATUS_SUCCESS) {
-        printf ("data upload failed");
-        cudaFree (devPtrC);
-        cublasDestroy(handle);
-        exit (EXIT_FAILURE);
-    }
-    // std::cout<<(C.str());
-    cudaFree (devPtrA);
-    cudaFree (devPtrB);
-	cudaFree (devPtrC);
-
-
 }
+
+Matrix ComputingUnit::dot(Matrix& A, Matrix& B){
+    CuMatrix devA(A,handle);
+    CuMatrix devB(B,handle);
+    CuMatrix devC=devA.dot(devB);
+    devC.updateMatrixFromGPU();
+}
+
 
 
 #endif
