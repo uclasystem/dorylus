d25bffe60951cb8e8348ed244c7ca13f674c9ed1
Jose <huguanzhou123@sina.com>
2019-08-21 18:06:31 -0700

    Fix parameters used for first benchmarking

--- miscs/generate-dataset/generate.py
+++ miscs/generate-dataset/generate.py
@@ -1,29 +1,33 @@
 import random
 
 
-dataset_dir = "../../../data/large/"
-num_vertices = 30000
-num_features = 60
-label_kinds = 12
+dataset_name = "large"
+num_vertices = 120000
+num_features = 602
+label_kinds = 41
+edge_probability = 0.0005
+dataset_dir = "../../../data/" + dataset_name + "/"
 
 
 # Generate graph.
-with open(dataset_dir + "large.graph", 'w+') as fgraph:
+with open(dataset_dir + dataset_name + ".graph", 'w+') as fgraph:
     for i in range(num_vertices):
         for j in range(num_vertices):
-            if random.random() > 0.99:
+            if random.random() < edge_probability:
                 fgraph.write(str(i) + " " + str(j) + "\n")
 
 print("Graph done.")
+
 
 # Generate initial features.
 with open(dataset_dir + "features", 'w+') as ffeats:
     for i in range(num_vertices):
         for j in range(num_features - 1):
             ffeats.write(str(random.random()) + ", ")
-        ffeats.write(str(random.random()) + "\n")
+        ffeats.write(str(random.random()) + "\n")exit
 
 print("Features done.")
+
 
 # Generate labels.
 with open(dataset_dir + "labels", 'w+') as flabels:
