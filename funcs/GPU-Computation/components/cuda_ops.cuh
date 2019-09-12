d94d475877b03c61531f1761726fa12b2105a2fe
shen teng <shen_teng@engineering.ucla.edu>
2019-09-12 14:50:30 -0700

    move functors to another file

---
+++ funcs/GPU-Computation/components/cuda_ops.cuh
@@ -0,0 +1,93 @@
+struct tanh_functor
+{
+    tanh_functor(){}
+    __host__ __device__
+        float operator()(const float& x) const { return tanhf(x);}
+};
+
+struct activateDerivate_functor
+{
+    activateDerivate_functor(){}
+    __host__ __device__
+        float operator()(const float& x) const { 
+            return 1 - pow(tanh(x), 2);
+        }
+};
+
+struct exp_functor
+{
+    exp_functor(){}
+    __host__ __device__
+        float operator()(const float& x) const { return expf(x);}
+};
+
+struct divide_functor
+{
+    divide_functor(float *denoms_):denoms(denoms_){}
+    __host__ __device__
+        float operator()(const int& i,const float& x) const { return x/denoms[i];}
+    float* denoms;
+};
+
+struct setRow_functor
+{
+    setRow_functor(unsigned col_):col(col_){}
+    unsigned col;
+    __host__ __device__
+        int operator()(const int& x) const { return x/col;}
+};
+
+struct setRowStarts{
+    setRowStarts(FeatType* data_ptr_,unsigned col_)
+        :col(col_),data_ptr(data_ptr_){}
+    __host__ __device__
+         FeatType* operator()(const int& i) const { 
+            return data_ptr + i*col;
+        }
+    FeatType* data_ptr;
+    unsigned col;
+};
+
+
+struct findRowMaximum{
+    findRowMaximum(unsigned col_)
+        :col(col_){}
+    __host__ __device__
+        unsigned operator()(const FeatType* ptr) const { 
+            unsigned index=0;
+            float max=ptr[0];
+            for (unsigned i=1;i<col;++i){
+                if(ptr[i]>max){
+                    index=i;
+                    max=ptr[i];
+                }
+            }
+            return index;
+        }
+    unsigned col;
+};
+
+struct findTrueLabel{
+    findTrueLabel(unsigned col_)
+        :col(col_){}
+    __host__ __device__
+        unsigned operator()(const FeatType* ptr) const { 
+           for(unsigned i=0;i<col;i++){
+            if(ptr[i]==1)
+                return i;
+           }
+           return (unsigned)-1;
+        }
+    unsigned col;
+};
+
+struct isEqual{
+    isEqual(){}
+    __host__ __device__
+        unsigned operator()(const unsigned& i,const unsigned&j) const { 
+            // printf("i: %u j: %u\n", i,j);
+           if(i==j) 
+            return 1;
+           else return 0;
+        }
+};
