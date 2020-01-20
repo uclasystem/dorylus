63014c6b83e0162e24ff8ca1804a3828e0e2f366
John <thorpedoes@g.ucla.edu>
2020-01-19 18:27:59 -0800

    Fixing bad white space (at least that was in this diff)

--- src/graph-server/GPU-Computation/compile.sh
+++ src/graph-server/GPU-Computation/compile.sh
@@ -11,7 +11,7 @@ a=`pwd`
 cd ../../common
 nvcc -std=c++11 -shared --compiler-options "-fPIC"  matrix.cpp  -I. -o libcommon.so -lcblas -lopenblas
 cd $a
-nvcc -shared --compiler-options "-fPIC -std=c++11" cu_matrix.cu -L../../common -lcommon -o libcumatrix.so -lcublas -lcudnn -lcblas  -lopenblas 
+nvcc -shared --compiler-options "-fPIC -std=c++11" cu_matrix.cu -L../../common -lcommon -o libcumatrix.so -lcublas -lcudnn -lcblas  -lopenblas
 nvcc -shared --compiler-options "-fPIC -std=c++11" comp_unit.cu -L../../common -lcommon -o libcu.so -lcublas -lcudnn -lcblas  -lopenblas
 nvcc test.cpp -L. -L../../common  -lcu -lcumatrix -lcommon -lcblas -lcublas -lopenblas -lcudnn -lcusparse
 
